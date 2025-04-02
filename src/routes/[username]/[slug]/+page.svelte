5f84616eedc3dc83f864636292a9ffef48635a51
Stanley Kurniawan <terserahskurniawan@gmail.com>
2025-04-02 00:10:04 -0700

    fixing animaiton by breaking reactivity of data.document

--- src/routes/[username]/[slug]/+page.svelte
+++ src/routes/[username]/[slug]/+page.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import Document from '$lib/components/Document.svelte';
+	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
+    const { document } = data;
+
 	// Set the document mode to 'read'
-	if (data.document && data.document.state) {
-		data.document.state.mode = 'read';
+	if (document && document.state) {
+		document.state.mode = 'read';
 	}
 </script>
 
-<Document node={data.document} />
+<Document node={document} />
