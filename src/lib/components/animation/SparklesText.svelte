62b45d98d5e30373bf6fc2ac014fac4cd7970094
Stanley Kurniawan <terserahskurniawan@gmail.com>
2025-04-27 11:03:33 -0700

    import error

--- src/lib/components/animation/SparklesText.svelte
+++ src/lib/components/animation/SparklesText.svelte
@@ -1,8 +1,8 @@
-<!-- credits to https://animation-svelte.vercel.app/magic/sparkles-text-->
-
 <script lang="ts">
 	import { cn } from '$lib/components/animation/utils';
-	import { Motion } from 'svelte-motion';
+	import { browser } from '$app/environment';
+	import { onMount, onDestroy } from 'svelte';
+	
 	interface Sparkle {
 		id: string;
 		x: string;
@@ -24,7 +24,8 @@
 	export { className as class };
 
 	let sparkles: Sparkle[] = [];
-	import { onMount, onDestroy } from 'svelte';
+	let Motion: any;
+	let motionLoaded = false;
 
 	let generateStar = () => {
 		let starX = `${Math.random() * 100}%`;
@@ -55,6 +56,18 @@
 
 	let interval: NodeJS.Timeout;
 	onMount(async () => {
+		// Only import and use svelte-motion on the client side
+		if (browser) {
+			try {
+				// Dynamic import of Motion component
+				const sveltemotion = await import('svelte-motion');
+				Motion = sveltemotion.Motion;
+				motionLoaded = true;
+			} catch (error) {
+				console.error("Failed to load Motion from svelte-motion:", error);
+			}
+		}
+		
 		initializeStars();
 		interval = setInterval(updateStars, 100);
 	});
@@ -69,31 +82,33 @@
 >
 	<span class="relative inline-block">
 		<!-- Sparkles layer (positioned absolutely, with pointer-events-none to allow clicking through) -->
-		{#each sparkles as item}
-			<Motion
-				initial={{ opacity: 0, left: item.x, top: item.y }}
-				animate={{
-					opacity: [0, 1, 0],
-					scale: [0, item.scale, 0],
-					rotate: [75, 120, 150]
-				}}
-				transition={{ duration: 0.8, repeat: Infinity, delay: item.delay }}
-				let:motion
-			>
-				<div
-					use:motion
-					class="pointer-events-none absolute z-20"
-					style="left: {item.x}px; top: {item.y}px;"
+		{#if browser && motionLoaded}
+			{#each sparkles as item}
+				<Motion
+					initial={{ opacity: 0, left: item.x, top: item.y }}
+					animate={{
+						opacity: [0, 1, 0],
+						scale: [0, item.scale, 0],
+						rotate: [75, 120, 150]
+					}}
+					transition={{ duration: 0.8, repeat: Infinity, delay: item.delay }}
+					let:motion
 				>
-					<svg id={item.id} width="21" height="21" viewBox="0 0 21 21">
-						<path
-							d="M9.82531 0.843845C10.0553 0.215178 10.9446 0.215178 11.1746 0.843845L11.8618 2.72026C12.4006 4.19229 12.3916 6.39157 13.5 7.5C14.6084 8.60843 16.8077 8.59935 18.2797 9.13822L20.1561 9.82534C20.7858 10.0553 20.7858 10.9447 20.1561 11.1747L18.2797 11.8618C16.8077 12.4007 14.6084 12.3916 13.5 13.5C12.3916 14.6084 12.4006 16.8077 11.8618 18.2798L11.1746 20.1562C10.9446 20.7858 10.0553 20.7858 9.82531 20.1562L9.13819 18.2798C8.59932 16.8077 8.60843 14.6084 7.5 13.5C6.39157 12.3916 4.19225 12.4007 2.72023 11.8618L0.843814 11.1747C0.215148 10.9447 0.215148 10.0553 0.843814 9.82534L2.72023 9.13822C4.19225 8.59935 6.39157 8.60843 7.5 7.5C8.60843 6.39157 8.59932 4.19229 9.13819 2.72026L9.82531 0.843845Z"
-							fill={item.color}
-						/>
-					</svg>
-				</div>
-			</Motion>
-		{/each}
+					<div
+						use:motion
+						class="pointer-events-none absolute z-20"
+						style="left: {item.x}px; top: {item.y}px;"
+					>
+						<svg id={item.id} width="21" height="21" viewBox="0 0 21 21">
+							<path
+								d="M9.82531 0.843845C10.0553 0.215178 10.9446 0.215178 11.1746 0.843845L11.8618 2.72026C12.4006 4.19229 12.3916 6.39157 13.5 7.5C14.6084 8.60843 16.8077 8.59935 18.2797 9.13822L20.1561 9.82534C20.7858 10.0553 20.7858 10.9447 20.1561 11.1747L18.2797 11.8618C16.8077 12.4007 14.6084 12.3916 13.5 13.5C12.3916 14.6084 12.4006 16.8077 11.8618 18.2798L11.1746 20.1562C10.9446 20.7858 10.0553 20.7858 9.82531 20.1562L9.13819 18.2798C8.59932 16.8077 8.60843 14.6084 7.5 13.5C6.39157 12.3916 4.19225 12.4007 2.72023 11.8618L0.843814 11.1747C0.215148 10.9447 0.215148 10.0553 0.843814 9.82534L2.72023 9.13822C4.19225 8.59935 6.39157 8.60843 7.5 7.5C8.60843 6.39157 8.59932 4.19229 9.13819 2.72026L9.82531 0.843845Z"
+								fill={item.color}
+							/>
+						</svg>
+					</div>
+				</Motion>
+			{/each}
+		{/if}
 
 		<slot></slot>
 	</span>
